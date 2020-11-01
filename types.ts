6f4c8804f485010324cbaa7c0c36b671f455ac8d
giaedgington <gia.edgington@gmail.com>
2020-11-01 18:31:40 -0500

    Add basic types

---
+++ types.ts
@@ -0,0 +1,21 @@
+var n : Number = 1;
+
+var isFall : boolean = true;
+
+var name : String = 'Bran';
+
+var names : String[] = ["Abril", "Franchely", "Gia"];
+
+var cohort : any[] = ["Abril", "Franchely", "Gia",3];
+
+enum Starks { Jon, Bran, Eddard, Catlyn};
+
+var cat : Starks = Starks.Catlyn;
+
+var tuple : [string, number];
+
+tuple = ["hello", 7]
+
+function getName() : string{
+    return 'Bran';
+}
